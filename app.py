5f5c9a1189437b318d8f5040663c20bc92374a14
Ubuntu <ubuntu@ip-172-31-87-249.ec2.internal>
2019-07-01 21:34:47 +0000

    added styles and add and view html pages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'cook_book'
-app.config["MONGO_URI"] = 'mongodb+srv://root:root@myfirstcluster-vay7y.mongodb.net/cook_book?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv ('MONGO_URI','mongodb+srv://root:root@myfirstcluster-vay7y.mongodb.net/cook_book?retryWrites=true&w=majority')
 
 mongo = PyMongo(app)
 
@@ -14,9 +14,14 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
-
+@app.route('/add_recipes')
+def add_recipes():
+    return render_template("addrecipes.html")
+@app.route('/get_catagories')
+def get_catagories():
+    return render_template("catagories.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-debug=True)
+            debug=True)
